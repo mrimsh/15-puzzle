70e3ad17752a6897f441da3fb0bf6a1bbb44d743
Pavel Borobov <bkollleu@gmail.com>
2013-05-04 14:21:22 +0400

    Creating methods Swap and Move.
    Finally complete my work.
    Swapping two tiles without change the links.

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -17,9 +17,42 @@ public class Tile : MonoBehaviour
 	void Update ()
 	{
 	}
-	
+	void Swap(Tile t)
+	{
+		int tempNumber = t.number;
+		Material tempMaterial = t.renderer.material;
+		
+		t.number = this.number;
+		this.number = tempNumber;
+		t.renderer.material = this.renderer.material;
+		this.renderer.material = tempMaterial;
+	}
+	bool Move()
+	{
+		if(left != null && left.number == 0)
+		{
+			Swap(left);
+			return true;
+		} else if(right != null && right.number == 0)
+		{
+			Swap(right);
+			return true;
+		} else if(top != null && top.number == 0)
+		{
+			Swap(top);
+			return true;
+		} else if(bottom != null && bottom.number == 0)
+		{
+			Swap(bottom);
+			return true;
+		} else return false;
+		
+	}
 	void OnMouseDown ()
 	{
-		Debug.Log (number);
+		if(Move ())
+		{
+			Debug.Log (number);
+		}
 	}
 }
